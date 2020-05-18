722b9be7286fc75a5d1df20e973d6a72622f7f31
Michelangelo Esposito <michelangeloesposito24@gmail.com>
2020-05-18 22:57:33 +0200

    Add files via upload

--- App/Django server/UploadImage/models.py
+++ App/Django server/UploadImage/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
 
+def get_filename(instance, filename):
+    extension = filename.split('.')[-1]
+    return 'media.jpg'
+
+
 class UploadImage(models.Model):
-    image = models.ImageField(blank=False, null=False)
+    image = models.ImageField(upload_to=get_filename, blank=False, null=False)
 
     def __str__(self):
         return self.image.name
