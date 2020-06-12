5c7e29a77d1885ffa9c0a6d48848e46881c5a1f7
Michelangelo Esposito <michelangeloesposito24@gmail.com>
2020-06-12 12:32:11 +0200

    Add files via upload

--- App/Android client/app/src/main/java/com/example/guidemear/activities/ArActivity.java
+++ App/Android client/app/src/main/java/com/example/guidemear/activities/ArActivity.java
@@ -4,21 +4,25 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ObjectAnimator;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.util.Log;
+import android.view.View;
 import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.guidemear.CustomArFragment;
-import com.example.guidemear.Painting;
+import com.example.guidemear.painting.Painting;
+import com.example.guidemear.painting.PaintingDetail;
 import com.example.guidemear.R;
 import com.example.guidemear.texttospeech.CustomUtteranceProgressListener;
 import com.example.guidemear.texttospeech.TextToSpeechManager;
@@ -43,15 +47,21 @@ import com.google.ar.sceneform.ux.TransformableNode;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.List;
 
 
 public class ArActivity extends AppCompatActivity {
     private static final String TAG = "ARActivity";
-    private boolean placeGuideModel = true;
-    private final Painting painting = getPainting();
+    private boolean placeGuideFlag = true;
+    private boolean playingFlag = false;
+    private Painting painting;
+    private int narrationIndex = 0;
+
+    private ImageButton playButton;
+    private Button nextButton;
+    private Button previousButton;
 
     private ArFragment arFragment;
     private AugmentedImage augmentedImage;
@@ -69,11 +79,13 @@ public class ArActivity extends AppCompatActivity {
             Log.d(TAG, "onTtsStartHandler. Message: " + message);
             String imagePath = message.getData().getString("IMAGE_PATH");
 
+
             try {
                 /*
                 ** Places the image in the scene and interpolates it between
                 ** the position in which it has been generated and a forward point
                  */
+
                 Pose imagePose = augmentedImage.getCenterPose();
                 Pose targetPose = imagePose.compose(Pose.makeTranslation(0, 0, 0));
                 Anchor anchor = arFragment
@@ -82,7 +94,6 @@ public class ArActivity extends AppCompatActivity {
                         .createAnchor(targetPose);
                 InputStream in = getAssets().open(imagePath + ".jpg");
                 Bitmap bitmap = BitmapFactory.decodeStream(in);
-
                 placeImageView(arFragment, anchor, bitmap);
 
                 if(imageNode != null) {
@@ -105,10 +116,7 @@ public class ArActivity extends AppCompatActivity {
                     objectAnimator.setDuration(2000);
                     objectAnimator.start();
                 }
-                else {
-                    Log.d(TAG, "ImageNode is null");
-                }
-            } catch (IOException e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
@@ -131,6 +139,19 @@ public class ArActivity extends AppCompatActivity {
                 imageNode.setParent(null);
                 imageNode = null;
             }
+
+            if(playingFlag) {
+                ttsManager.stop();
+                narrationIndex++;
+                if(narrationIndex >= painting.getPaintingInfo().size()) {
+                    narrationIndex = painting.getPaintingInfo().size() - 1;
+                }
+                PaintingDetail entry = painting.getPaintingInfo().get(narrationIndex);
+                ttsManager.speak(entry.getDescription(), entry.getImagePath());
+            }
+            else {
+                Log.d(TAG, "Cannot create ImageView, imageNode is null");
+            }
         }
     };
 
@@ -147,18 +168,71 @@ public class ArActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ar);
 
         arFragment = (CustomArFragment) getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment_2);
-        if(arFragment != null) {
-            arFragment.getPlaneDiscoveryController().hide();
-            arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdateFrame);
+        assert arFragment != null;
+        arFragment.getPlaneDiscoveryController().hide();
+        arFragment.getArSceneView().getScene().addOnUpdateListener(this::onUpdateFrame);
 
-            ttsManager = new TextToSpeechManager();
-            CustomUtteranceProgressListener progressListener = new CustomUtteranceProgressListener(onTtsStartHandler, onTtsDoneHandler);
-            ttsManager.initTts(getApplicationContext(), progressListener);
-        } else {
-            Toast.makeText(getApplicationContext(), "Could not start up the AR environment", Toast.LENGTH_SHORT).show();
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-        }
+        ttsManager = new TextToSpeechManager();
+        CustomUtteranceProgressListener progressListener = new CustomUtteranceProgressListener(onTtsStartHandler, onTtsDoneHandler);
+        ttsManager.initTts(getApplicationContext(), progressListener);
+
+        painting = getPainting();
+        List<PaintingDetail> paintingInfo = painting.getPaintingInfo();
+
+        /*
+        ** UI buttons listeners
+        */
+        playButton = findViewById(R.id.play_pause_button);
+        playButton.setVisibility(View.GONE);
+        playButton.setOnClickListener(v -> {
+            if (!playingFlag) {
+                /*
+                ** TTS is currently paused
+                ** Changes the ImageButton icon to the pause icon and resumes the narration
+                 */
+                Drawable icon = getDrawable(android.R.drawable.ic_media_pause);
+                playButton.setImageDrawable(icon);
+                playingFlag = true;
+
+                PaintingDetail entry = paintingInfo.get(narrationIndex);
+                ttsManager.speak(entry.getDescription(), entry.getImagePath());
+            } else {
+                /*
+                ** TTS is currently playing
+                ** Changes the ImageButton icon to the play icon and stops the narration
+                 */
+                Drawable icon = getDrawable(android.R.drawable.ic_media_play);
+                playButton.setImageDrawable(icon);
+                playingFlag = false;
+                ttsManager.stop();
+
+            }
+        });
+
+        nextButton = findViewById(R.id.next_button);
+        nextButton.setVisibility(View.GONE);
+        nextButton.setOnClickListener(v -> {
+            if(playingFlag) {
+                ttsManager.stop();
+                narrationIndex++;
+                if(narrationIndex >= paintingInfo.size()) {
+                    narrationIndex = paintingInfo.size() - 1;
+                }
+                PaintingDetail entry = paintingInfo.get(narrationIndex);
+                ttsManager.speak(entry.getDescription(), entry.getImagePath());
+            }
+        });
+
+        previousButton = findViewById(R.id.previous_button);
+        previousButton.setVisibility(View.GONE);
+        previousButton.setOnClickListener(v -> {
+            if(playingFlag) {
+                ttsManager.stop();
+                narrationIndex = (narrationIndex > 0 ? narrationIndex - 1 : 0);
+                PaintingDetail entry = paintingInfo.get(narrationIndex);
+                ttsManager.speak(entry.getDescription(), entry.getImagePath());
+            }
+        });
     }
 
 
@@ -179,7 +253,7 @@ public class ArActivity extends AppCompatActivity {
         /*
         ** First-time guide setup
          */
-        if(placeGuideModel) {
+        if(placeGuideFlag) {
             Collection<AugmentedImage> augmentedImages = frame.getUpdatedTrackables(AugmentedImage.class);
             augmentedImages.forEach(augmentedImage -> {
                 if(augmentedImage.getTrackingState() == TrackingState.TRACKING &&
@@ -191,12 +265,11 @@ public class ArActivity extends AppCompatActivity {
                     AnchorNode anchorNode = new AnchorNode(anchor);
                     anchorNode.setParent(arFragment.getArSceneView().getScene());
                     placeGuideModel(arFragment, anchor, Uri.parse("female_head.sfb"));
-                    placeGuideModel = false;
-                    narrate();
+                    placeGuideFlag = false;
+                    enableUI();
                 }
             });
         }
-
 
         /*
         ** Updates the guide model and the detail image to face the camera
@@ -215,6 +288,16 @@ public class ArActivity extends AppCompatActivity {
             Quaternion imageLookRotation = Quaternion.lookRotation(imageDirection, Vector3.up());
             imageNode.setWorldRotation(imageLookRotation);
         }
+    }
+
+
+    /**
+     * Enables the UI buttons after the image has been recognized
+     */
+    private void enableUI() {
+        playButton.setVisibility(View.VISIBLE);
+        nextButton.setVisibility(View.VISIBLE);
+        previousButton.setVisibility(View.VISIBLE);
     }
 
 
@@ -265,6 +348,18 @@ public class ArActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Error generating the image view: " + throwable.getMessage(), Toast.LENGTH_SHORT).show();
                     return null;
                 });
+    }
+
+
+    /**
+     *
+     */
+    private void destroyImageNode() {
+        if(imageNode != null) {
+            arFragment.getArSceneView().getScene().removeChild(imageNode);
+            imageNode.setParent(null);
+            imageNode = null;
+        }
     }
 
 
@@ -324,17 +419,6 @@ public class ArActivity extends AppCompatActivity {
 
 
     /**
-     * Iterates through the image-string pairs making up the painting info
-     * and feeds them to the TTS engine
-     */
-    private void narrate() {
-        painting.getPaintingInfo()
-                .entrySet()
-                .forEach(entry -> ttsManager.speak(entry.getValue(), entry.getKey()));
-    }
-
-
-    /**
      * Stops the TTS engine
      */
     @Override
@@ -350,27 +434,29 @@ public class ArActivity extends AppCompatActivity {
     ** Test painting setup
      */
     private Painting getPainting() {
-        Map<String, String> paintingInfo = new LinkedHashMap<>();
-        paintingInfo.put("default", "The main focus of the composition is the goddess of love and beauty, Venus. " +
+        List<PaintingDetail> paintingInfo = new ArrayList<>();
+
+
+        paintingInfo.add(new PaintingDetail ("default", "The main focus of the composition is the goddess of love and beauty, Venus. " +
                 "Born by the sea spray, she is blown on the island of Cyprus by the winds, Zephyr and Aura. " +
                 "She is met by a young woman, sometimes identified as the Hora of Spring, who holds a cloak covered in flowers " +
                 "and is ready to cover her. A detail often overlooked is the lack of shadows in the scene; " +
-                "according to some interpretations, the painting is set in an alternative reality, still very similar to our own.");
-        paintingInfo.put("Venus", "The goddess is standing on a giant scallop shell, as pure and perfect as a pearl. " +
+                "according to some interpretations, the painting is set in an alternative reality, still very similar to our own."));
+        paintingInfo.add(new PaintingDetail("Venus", "The goddess is standing on a giant scallop shell, as pure and perfect as a pearl. " +
                 "She covers her nakedness with long, blond hair, which has reflections of light from the fact it has been gilded. " +
                 "The fine modelling and white flesh colour gives her the appearance of a statue, an impression fortified by her stance, " +
-                "which is very similar to the Venus Pudica, an ancient statue of the greek-roman period.");
-        paintingInfo.put("Shell", "You may wonder why Venus is standing on a shell; the story goes that the God Uranus had a son named Chronus, " +
+                "which is very similar to the Venus Pudica, an ancient statue of the greek-roman period."));
+        paintingInfo.add(new PaintingDetail("Shell", "You may wonder why Venus is standing on a shell; the story goes that the God Uranus had a son named Chronus, " +
                 "who overthrew his father and threw his genitals into the sea; this caused the water to be fertilised, " +
-                "and thus the goddess was born.");
-        paintingInfo.put("Zephyrus", "In the top left of the piece we can notice Zephyrus, god of the winds; " +
+                "and thus the goddess was born."));
+        paintingInfo.add(new PaintingDetail("Zephyrus", "In the top left of the piece we can notice Zephyrus, god of the winds; " +
                 "he is  holding Aura, personification of a light breeze. The two are highlighting the pale face of the goddess, " +
-                "while blowing the shell towards the coast.");
-        paintingInfo.put("Aura", "The Hora herself may be a complementary version of the nymph Chloris. " +
+                "while blowing the shell towards the coast."));
+        paintingInfo.add(new PaintingDetail("Aura", "The Hora herself may be a complementary version of the nymph Chloris. " +
                 "Are they two versions of the same person then? It might be; the story of this woman is narrated in " +
                 "“I Fasti” by latin author Ovidio and the painted in “The Spring”, by Botticelli himself, " +
                 "where the woman gets kidnapped by Zephyrus to become a mystical figure. The theory is quite farfetched, " +
-                "however there’s a detail in its favour: the roses falling around her and Zephyrus.");
+                "however there’s a detail in its favour: the roses falling around her and Zephyrus."));
         return new Painting("Sandro Botticelli", "La nascita di Venere", paintingInfo);
     }
 }
