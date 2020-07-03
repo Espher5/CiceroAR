c78e48ec7943a4c18721d9842fdf309de09b0426
Michelangelo Esposito <michelangeloesposito24@gmail.com>
2020-07-03 14:05:48 +0200

    Add files via upload

--- App/Android client/app/src/main/java/com/example/guidemear/activities/ImagePreviewActivity.java
+++ App/Android client/app/src/main/java/com/example/guidemear/activities/ImagePreviewActivity.java
@@ -5,19 +5,33 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.example.guidemear.Painting;
+import com.example.guidemear.network.SingletonAsyncDownloadTask;
+import com.example.guidemear.painting.Painting;
 import com.example.guidemear.R;
 import com.example.guidemear.network.UploadAPI;
 import com.example.guidemear.network.UploadHandler;
+import com.example.guidemear.painting.PaintingDetail;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
@@ -34,7 +48,8 @@ import retrofit2.Retrofit;
  */
 public class ImagePreviewActivity extends AppCompatActivity {
     private static final String TAG = "ImagePreviewActivity";
-
+    private ProgressBar progressBar;
+    private Bitmap[] downloadResult;
 
     /**
      * Retrieves the path of the selected image from the intent,
@@ -59,8 +74,14 @@ public class ImagePreviewActivity extends AppCompatActivity {
             imagePreview.setImageURI(Uri.fromFile(file));
         }
 
+        progressBar = findViewById(R.id.progressBar_cyclic);
+        progressBar.setVisibility(View.GONE);
+
         Button uploadButton = findViewById(R.id.upload_button);
-        uploadButton.setOnClickListener(v -> uploadImage(imagePath));
+        uploadButton.setOnClickListener(v -> {
+            progressBar.setVisibility(View.VISIBLE);
+            uploadImage(imagePath);
+        });
 
         Button cancelButton = findViewById(R.id.cancel_button);
         cancelButton.setOnClickListener(v -> {
@@ -81,29 +102,41 @@ public class ImagePreviewActivity extends AppCompatActivity {
         RequestBody requestBody = RequestBody.create(MediaType.parse("image/*"), file);
         MultipartBody.Part part = MultipartBody.Part.createFormData("image", file.getName(), requestBody);
 
+        Toast.makeText(getApplicationContext(), "Uploading image...", Toast.LENGTH_LONG).show();
+
         Retrofit retrofit = UploadHandler.getRetrofit();
         UploadAPI uploadAPI = retrofit.create(UploadAPI.class);
         Call<Painting> call = uploadAPI.uploadImage(part);
         call.enqueue(new Callback<Painting>() {
             @Override
             public void onResponse(@NonNull Call<Painting> call, @NonNull Response<Painting> response) {
+                progressBar.setVisibility(View.GONE);
                 if(response.body() == null) {
                     Log.e(TAG, "Response error: no body in response");
                 }
                 else {
-                    Painting painting = new Painting(
-                            response.body().getArtist(),
-                            response.body().getTitle(),
-                            null
-                    );
+                    Toast.makeText(getApplicationContext(), "Image successfully uploaded", Toast.LENGTH_SHORT).show();
+                    List<PaintingDetail> paintingDetails = response.body().getPaintingDetails();
+
+                    List<String> urls = new ArrayList<>();
+                    List<String> descriptions = new ArrayList<>();
+                    paintingDetails.forEach(entry -> {
+                        urls.add(entry.getImagePath());
+                        descriptions.add(entry.getDescription());
+                    });
+                    new SingletonAsyncDownloadTask().getInstance().execute(urls.toArray(new String[0]));
+
                     Intent intent = new Intent(getApplicationContext(), ArActivity.class);
-                    intent.putExtra("painting", painting);
+                    intent.putExtra("artist", response.body().getArtist());
+                    intent.putExtra("title", response.body().getTitle());
+                    intent.putExtra("descriptions", descriptions.toArray(new String[0]));
                     startActivity(intent);
                 }
             }
 
             @Override
             public void onFailure(@NonNull Call<Painting> call, @NonNull Throwable t) {
+                progressBar.setVisibility(View.GONE);
                 t.printStackTrace();
                 Toast.makeText(getApplicationContext(), "Error! No response from server", Toast.LENGTH_SHORT).show();
             }
